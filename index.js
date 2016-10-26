af3a5d6f018b2550c9b2612de775b69c1e9efefb
Abhinav Adduri <aadduri@berkeley.edu>
2016-10-25 23:54:08 -0700

    bug fix for seed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
-const React = require('react')
 
 // serve static assets normally
 app.use(express.static(__dirname))
